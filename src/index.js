966090295ac7a800b4024d08a9f31f13605d46cc
Orion2008 <barjamajor@gmail.com>
2024-09-29 00:28:48 +0200

    Added js code for the user to switch between pages

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+http.createServer(function(req, res) {
+    const pathname = url.parse(req.url, true);
+    const filename = pathname.pathname.replace('.html', '');
+    console.log('Pathname -> ', pathname.pathname);
+    console.log('Filename -> ', filename);
+    fs.readFile(`./pages${filename}.html`, function(err, data){
+        if(err){
+            fs.readFile('./pages/404.html', function(err2, data2){
+                if(err2){
+                    res.writeHead(404, {'Content-Type' : 'text/html'})
+                    res.write('404 error not found');
+                    return res.end();
+                }else {
+                    res.writeHead(404, {'Content-Type' : 'text/html'})
+                    res.write(data2);
+                    return res.end();
+                }
+            })
+        }else {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.write(data);
+            return res.end();
+        }
+    });
+}).listen(8080);
